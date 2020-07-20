4b23128c88b1fd3acb35f199c6b22ad51d989cea
Yuxing Li (Jackie) <jackie.li@fuseim.com>
2020-07-20 16:17:35 +0100

    export binary encoder & decoder

--- js/commonjs/export.js
+++ js/commonjs/export.js
@@ -10,19 +10,28 @@
 goog.provide('jspb.Export');
 
 goog.require('goog.object');
+goog.require('jspb.BinaryDecoder');
+goog.require('jspb.BinaryEncoder');
 goog.require('jspb.BinaryReader');
 goog.require('jspb.BinaryWriter');
 goog.require('jspb.ExtensionFieldBinaryInfo');
 goog.require('jspb.ExtensionFieldInfo');
 goog.require('jspb.Message');
 goog.require('jspb.Map');
+goog.require('jspb.arith.Int64');
+goog.require('jspb.arith.UInt64');
 
 exports.Map = jspb.Map;
 exports.Message = jspb.Message;
+exports.BinaryDecoder = jspb.BinaryDecoder;
+exports.BinaryEncoder = jspb.BinaryEncoder;
 exports.BinaryReader = jspb.BinaryReader;
 exports.BinaryWriter = jspb.BinaryWriter;
 exports.ExtensionFieldInfo = jspb.ExtensionFieldInfo;
 exports.ExtensionFieldBinaryInfo = jspb.ExtensionFieldBinaryInfo;
+exports.arith = {}
+exports.arith.Int64 = jspb.arith.Int64
+exports.arith.UInt64 = jspb.arith.UInt64
 
 // These are used by generated code but should not be used directly by clients.
 exports.exportSymbol = goog.exportSymbol;
